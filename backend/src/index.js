27a527c75e93f952456ad34db9086e9cfbc983e8
lenatisha <168923134+lenatisha@users.noreply.github.com>
2024-05-04 23:10:20 +0300

    chore: add production build port of react client to cors allowed list

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow frontend to connect
+    origin: ["http://localhost:5173",['http://localhost:4173']], // Allow frontend to connect
     credentials: true, // Crucial for cookies
   })
 );
